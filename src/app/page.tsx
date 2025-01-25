028dcbec7146251dc3dcf71fec2ca0e5a1cf6c8f
Simón Cáceres <secaceres@ticmoai.com>
2025-01-24 23:26:30 -0300

    feature: first steps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,204 @@
+"use client";
+import React, { useState, Suspense } from "react";
+import dynamicRender from "next/dynamic";
+
 import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = dynamicRender(() => import("../components/navbar"), {
+  ssr: false,
+});
+const Switcher = dynamicRender(() => import("../components/switcher"), {
+  ssr: false,
+});
+const Tagline = dynamicRender(() => import("../components/tagline"), {
+  ssr: false,
+});
+const Form = dynamicRender(() => import("../components/form"), { ssr: false });
+const About = dynamicRender(() => import("../components/about"), {
+  ssr: false,
+});
+const Client = dynamicRender(() => import("../components/client"), {
+  ssr: false,
+});
+const Blogs = dynamicRender(() => import("../components/blogs"), {
+  ssr: false,
+});
+const Footer = dynamicRender(() => import("../components/footer"), {
+  ssr: false,
+});
+
+import { packages } from "@/data/data";
+
+const FiMapPin = dynamicRender(
+  () => import("../assets/icons/vander").then((mod) => mod.FiMapPin),
+  { ssr: false }
+);
+
+export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isOpen, sehrefpen] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Tagline />
+      <Navbar
+        navclass="defaultscroll is-sticky tagline-height"
+        navlight={true}
+        manuclass="justify-end nav-light"
+      />
+      <section className="relative md:pt-72 md:pb-60 py-36 table w-full items-center bg-[url('../assets/images/bg/1.jpg')] bg-hrefp bg-no-repeat bg-cover">
+        <div className="absolute inset-0 bg-slate-900/40"></div>
+        <div className="container relative">
+          <div className="grid md:grid-cols-12 grid-cols-1 items-center mt-10 gap-[30px]">
+            <div className="lg:col-span-8 md:col-span-7 md:order-1 order-2">
+              <h5 className="text-3xl font-dancing text-white">
+                Beauty of Discovers
+              </h5>
+              <h4 className="font-bold text-white lg:leading-normal leading-normal text-4xl lg:text-6xl mb-6 mt-5">
+                Let's Leave The Road, <br /> And Take The Travosy
+              </h4>
+              <p className="text-white/70 text-xl max-w-xl">
+                Planning for a trip? We will organize your trip with the best
+                places and within best budget!
+              </p>
+            </div>
+
+            <div className="lg:col-span-4 md:col-span-5 md:text-center md:order-2 order-1">
+              <Link
+                href="#!"
+                onClick={() => sehrefpen(true)}
+                className="lightbox lg:h-24 h-20 lg:w-24 w-20 rounded-full shadow-lg dark:shadow-gray-800 inline-flex items-center justify-center bg-white hover:bg-red-500 text-red-500 hover:text-white duration-500 ease-in-out mx-auhref"
+              >
+                <i className="mdi mdi-play inline-flex items-center justify-center text-3xl"></i>
+              </Link>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+
+      <div className="container relative -mt-16 z-1">
+        <Form />
+      </div>
+
+      <section className="relative md:py-24 py-16 overflow-hidden">
+        <div className="container relative md:mt-24 mt-16">
+          <div className="grid grid-cols-1 pb-8 text-center">
+            <h3 className="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">
+              hrefurs Packages
+            </h3>
+
+            <p className="text-slate-400 max-w-xl mx-auhref">
+              Planning for a trip? We will organize your trip with the best
+              places and within best budget!
+            </p>
+          </div>
+
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-6 gap-6">
+            {packages.slice(0, 6).map((item, index) => {
+              return (
+                <div
+                  className="group rounded-md shadow dark:shadow-gray-700"
+                  key={index}
+                >
+                  <div className="relative overflow-hidden rounded-t-md shadow dark:shadow-gray-700 mx-3 mt-3">
+                    <Image
+                      src={item.image}
+                      className="scale-125 group-hover:scale-100 duration-500"
+                      alt=""
+                    />
+                    {item.tagText && (
+                      <div className="absolute hrefp-0 start-0 p-4">
+                        <span className="bg-red-500 text-white text-[12px] px-2.5 py-1 font-medium rounded-md h-5">
+                          {item.tagText}
+                        </span>
+                      </div>
+                    )}
+
+                    <div className="absolute hrefp-0 end-0 p-4">
+                      <Link
+                        href="#"
+                        className="size-8 inline-flex justify-center items-center bg-white dark:bg-slate-900 shadow dark:shadow-gray-800 rounded-full text-slate-100 dark:text-slate-700 focus:text-red-500 dark:focus:text-red-500 hover:text-red-500 dark:hover:text-red-500"
+                      >
+                        <i className="mdi mdi-heart text-[20px] align-middle"></i>
+                      </Link>
+                    </div>
+                  </div>
+
+                  <div className="p-4">
+                    <p className="flex items-center text-slate-400 font-medium mb-2">
+                      <FiMapPin className="text-red-500 size-4 me-1"></FiMapPin>{" "}
+                      {item.place}
+                    </p>
+                    <Link
+                      href={`/hrefur-detail-one/${item.id}`}
+                      className="text-lg font-medium hover:text-red-500 duration-500 ease-in-out"
+                    >
+                      {item.title}
+                    </Link>
+
+                    <div className="flex items-center mt-2">
+                      <span className="text-slate-400">Rating:</span>
+                      <ul className="text-lg font-medium text-amber-400 list-none ms-2 space-x-1">
+                        <li className="inline">
+                          <i className="mdi mdi-star align-middle"></i>
+                        </li>
+                        <li className="inline">
+                          <i className="mdi mdi-star align-middle"></i>
+                        </li>
+                        <li className="inline">
+                          <i className="mdi mdi-star align-middle"></i>
+                        </li>
+                        <li className="inline">
+                          <i className="mdi mdi-star align-middle"></i>
+                        </li>
+                        <li className="inline">
+                          <i className="mdi mdi-star align-middle"></i>
+                        </li>
+                        <li className="inline text-black dark:text-white text-sm">
+                          5.0(30)
+                        </li>
+                      </ul>
+                    </div>
+
+                    <div className="mt-4 pt-4 flex justify-between items-center border-t border-slate-100 dark:border-gray-800">
+                      <h5 className="text-lg font-medium text-red-500">
+                        $ 58 / Day
+                      </h5>
+
+                      <Link
+                        href=""
+                        className="text-slate-400 hover:text-red-500"
+                      >
+                        Explore Now <i className="mdi mdi-arrow-right"></i>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="mt-6 text-center">
+            <Link
+              href="/grid-right-sidebar"
+              className="text-slate-400 hover:text-red-500 inline-block"
+            >
+              See More hrefurs{" "}
+              <i className="mdi mdi-arrow-right align-middle"></i>
+            </Link>
+          </div>
+        </div>
+
+        <About />
+
+        <Client />
+
+        <Blogs />
+      </section>
+      <Footer />
+      <Switcher />
+    </Suspense>
   );
 }
---
+++ src/components/switcher.jsx
@@ -0,0 +1,110 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+
+import { FiSun, FiMoon } from "../assets/icons/vander";
+export default function Switcher() {
+  let [visible, setVisible] = useState(false);
+  let htmlTag;
+
+  useEffect(() => {
+    if (typeof document !== "undefined") {
+      htmlTag = document.getElementsByTagName("html")[0];
+    }
+  }, []);
+
+  const changeTheme = () => {
+    if (htmlTag) {
+      if (htmlTag.className.includes("dark")) {
+        htmlTag.className = "light";
+      } else {
+        htmlTag.className = "dark";
+      }
+    }
+  };
+
+  const modeChange = () => {
+    if (htmlTag) {
+      const switcherRtl = document.getElementById("switchRtl");
+      if (switcherRtl.innerText === "LTR") {
+        htmlTag.dir = "ltr";
+      } else {
+        htmlTag.dir = "rtl";
+      }
+    }
+  };
+
+  const toggleVisible = () => {
+    if (typeof document !== "undefined") {
+      const scrolled = document.documentElement.scrollTop;
+      if (scrolled > 300) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (typeof document === "undefined") return;
+    if (typeof window !== "undefined") {
+      const scrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      };
+
+      window.addEventListener("scroll", toggleVisible);
+
+      // Cleanup event listener
+      return () => {
+        window.removeEventListener("scroll", toggleVisible);
+      };
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="fixed top-1/4 -left-2 z-50">
+        <span className="relative inline-block rotate-90">
+          <input
+            type="checkbox"
+            className="checkbox opacity-0 absolute"
+            id="chk"
+            onChange={(e) => changeTheme(e)}
+          />
+          <label
+            className="label bg-slate-900 dark:bg-white shadow dark:shadow-gray-800 cursor-pointer rounded-full flex justify-between items-center p-1 w-14 h-8"
+            htmlFor="chk"
+          >
+            <FiMoon className="w-[18px] h-[18px] text-yellow-500"></FiMoon>
+            <FiSun className="w-[18px] h-[18px] text-yellow-500"></FiSun>
+            <span className="ball bg-white dark:bg-slate-900 rounded-full absolute top-[2px] left-[2px] w-7 h-7"></span>
+          </label>
+        </span>
+      </div>
+
+      <div className="fixed top-[40%] -left-3 z-50">
+        <Link href="" id="switchRtl" onClick={() => modeChange()}>
+          <span className="py-1 px-3 relative inline-block rounded-b-md -rotate-90 bg-white dark:bg-slate-900 shadow-md dark:shadow dark:shadow-gray-800 font-semibold rtl:block ltr:hidden">
+            LTR
+          </span>
+          <span className="py-1 px-3 relative inline-block rounded-b-md -rotate-90 bg-white dark:bg-slate-900 shadow-md dark:shadow dark:shadow-gray-800 font-semibold ltr:block rtl:hidden">
+            RTL
+          </span>
+        </Link>
+      </div>
+
+      <Link
+        href="#"
+        onClick={() => scrollToTop()}
+        id="back-to-top"
+        className="back-to-top fixed text-lg rounded-md z-10 bottom-5 end-5 size-8 text-center bg-red-500/10 hover:bg-red-500 text-red-500 hover:text-white justify-center items-center"
+        style={{ display: visible ? "inline-flex" : "none" }}
+      >
+        <i className="mdi mdi-arrow-up"></i>
+      </Link>
+    </>
+  );
+}
